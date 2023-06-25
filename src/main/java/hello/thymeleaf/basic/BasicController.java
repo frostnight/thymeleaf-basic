6c25f897b89631d7b4027d7c82dcbbb3ad72ebc9
frostnight <frostnightsong@gmail.com>
2023-06-25 20:39:36 +0900

    block

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -114,6 +114,12 @@ public class BasicController {
 		return "basic/comments";
 	}
 
+	@GetMapping("/block")
+	public String block(Model model){
+		addUsers(model);
+		return "basic/block";
+	}
+
 	private void addUsers(Model model){
 		List<User> users = List.of(
 			new User("UserA", 10),
